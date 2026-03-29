ffa0702043e1270e36f2d7111254492e004f6c20
Helio <helio_wojciki@hotmail.com>
2026-03-29 02:33:56 -0300

    feat: cria endpoint de teste para verificar api

---
+++ backend-java/src/main/java/com/helio/comandas_api/controller/TesteController.java
@@ -0,0 +1,13 @@
+package com.helio.comandas_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "API de comandas rodando com sucesso";
+    }
+}
