1359d1f02a150b47bb67d4792a54fe325b1734a8
Helio <helio_wojciki@hotmail.com>
2026-03-29 01:19:12 -0300

    chore: estrutura inicial do backend java

---
+++ backend-java/src/main/java/com/helio/comandas_api/ComandasApiApplication.java
@@ -0,0 +1,13 @@
+package com.helio.comandas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandasApiApplication.class, args);
+	}
+
+}
